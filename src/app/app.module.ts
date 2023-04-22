7c81d064f7c5206540e3ff6f55f3fdc5e4e63fa4
Laurence Cortez <laurencecortez.com>
2023-04-22 11:48:44 +0800

    setup components and tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,25 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { provideStorage,getStorage } from '@angular/fire/storage';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
+    ComponentsModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -6,11 +6,40 @@ const routes: Routes = [
   {
     path: '',
     component: HomePage,
-  }
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home',
+      },
+      {
+        path: 'home',
+        loadChildren: () =>
+          import('./tabs/home/home.module').then((m) => m.HomePageModule),
+      },
+      {
+        path: 'radio',
+        loadChildren: () =>
+          import('./tabs/radio/radio.module').then((m) => m.RadioPageModule),
+      },
+      {
+        path: 'library',
+        loadChildren: () =>
+          import('./tabs/library/library.module').then(
+            (m) => m.LibraryPageModule
+          ),
+      },
+      {
+        path: 'search',
+        loadChildren: () =>
+          import('./tabs/search/search.module').then((m) => m.SearchPageModule),
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class HomePageRoutingModule {}
