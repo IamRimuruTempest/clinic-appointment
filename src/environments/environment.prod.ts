94b8e8d8c1e19bba01b9c02d037144749c84237a
Laurence Cortez <laurencecortez.com>
2023-04-22 10:55:31 +0800

    connected to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'clinic-app-82c47',
+    appId: '1:150857360799:web:03d40703f8a2d761e789aa',
+    storageBucket: 'clinic-app-82c47.appspot.com',
+    apiKey: 'AIzaSyDX6w_yVeEb8nE1RnJe99iHVJIdeLnZH6I',
+    authDomain: 'clinic-app-82c47.firebaseapp.com',
+    messagingSenderId: '150857360799',
+  },
   production: true
 };
