1a3d0471ff078e03431889e76b5eadd64d354906
Laurence Cortez <laurencecortez.com>
2023-04-24 08:16:43 +0800

    fixed input reactive forms bug

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputComponent } from './input/input.component';
 import { IonicModule } from '@ionic/angular';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [InputComponent],
-  imports: [CommonModule, IonicModule],
+  imports: [CommonModule, IonicModule, ReactiveFormsModule],
   exports: [InputComponent],
 })
 export class ComponentsModule {}
