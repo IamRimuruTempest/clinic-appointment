22ff059618ee13b938bf9c6d94abfc25803d63be
Laurence Cortez <laurencecortez.com>
2023-05-10 23:03:35 +0800

    list inventory

--- src/app/admin/tabs/inventory/inventory.page.ts
+++ src/app/admin/tabs/inventory/inventory.page.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { AddInventoryComponent } from './add-inventory/add-inventory.component';
+import { DataService } from 'src/app/services/data.service';
+import { Inventory } from 'src/app/interfaces/inventory.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-inventory',
   templateUrl: './inventory.page.html',
   styleUrls: ['./inventory.page.scss'],
 })
-export class InventoryPage implements OnInit {
-  constructor(private modalCtrl: ModalController) {}
+export class InventoryPage implements OnInit, OnDestroy {
+  inventoryList: Inventory[] = [];
+  inventorySub: Subscription;
+  constructor(
+    private modalCtrl: ModalController,
+    private dataService: DataService
+  ) {
+    this.inventorySub = this.dataService
+      .getInventories()
+      .subscribe((data) => (this.inventoryList = data));
+  }
 
   ngOnInit() {}
+
+  ngOnDestroy() {
+    this.inventorySub.unsubscribe();
+  }
 
   async openAddModal() {
     const modal = await this.modalCtrl.create({
@@ -19,4 +35,8 @@ export class InventoryPage implements OnInit {
     });
     modal.present();
   }
+
+  updateInventory(item: Inventory) {}
+
+  viewInventory(item: Inventory) {}
 }
