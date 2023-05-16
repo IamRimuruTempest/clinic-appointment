719427c7c7dc0ac01b4aaf93300461f456dc7620
Laurence Cortez <laurencecortez.com>
2023-05-16 20:02:29 +0800

    created select component reactive

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { InputComponent } from './input/input.component';
 import { IonicModule } from '@ionic/angular';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SelectComponent } from './select/select.component';
 
 @NgModule({
-  declarations: [InputComponent],
+  declarations: [InputComponent, SelectComponent],
   imports: [CommonModule, IonicModule, ReactiveFormsModule],
-  exports: [InputComponent],
+  exports: [InputComponent, SelectComponent],
 })
 export class ComponentsModule {}
