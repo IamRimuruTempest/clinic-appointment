6dc3ab3f5189c0508f9e659ab50d65cac9704103
Laurence Cortez <laurencecortez.com>
2023-06-05 00:51:26 +0800

    client firebase credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,14 @@
 
 export const environment = {
   firebase: {
-    projectId: 'clinic-app-82c47',
-    appId: '1:150857360799:web:03d40703f8a2d761e789aa',
-    storageBucket: 'clinic-app-82c47.appspot.com',
-    apiKey: 'AIzaSyDX6w_yVeEb8nE1RnJe99iHVJIdeLnZH6I',
-    authDomain: 'clinic-app-82c47.firebaseapp.com',
-    messagingSenderId: '150857360799',
+    apiKey: 'AIzaSyDPw0HLt9zwHm69UDEL-p5Zs6Wj-8iU-M4',
+    authDomain: 'dental-clinic-fcba5.firebaseapp.com',
+    projectId: 'dental-clinic-fcba5',
+    storageBucket: 'dental-clinic-fcba5.appspot.com',
+    messagingSenderId: '806851107082',
+    appId: '1:806851107082:web:5d8b2a8fa856dbf5cdc2f3',
   },
-  production: false
+  production: false,
 };
 
 /*
