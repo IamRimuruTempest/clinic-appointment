c48424fca6a855d7cb958ef389d122cb2205d072
Laurence Cortez <laurencecortez.com>
2023-06-07 23:35:57 +0800

    improve view inventory

--- src/app/admin/tabs/inventory/view-inventory/view-inventory.component.ts
+++ src/app/admin/tabs/inventory/view-inventory/view-inventory.component.ts
@@ -27,4 +27,15 @@ export class ViewInventoryComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.historySub!.unsubscribe();
   }
+
+  public formatDate(t: Date): string {
+    const date = ('0' + t.getDate()).slice(-2);
+    const month = ('0' + (t.getMonth() + 1)).slice(-2);
+    const year = t.getFullYear();
+    const hours = ('0' + t.getHours()).slice(-2);
+    const minutes = ('0' + t.getMinutes()).slice(-2);
+    const seconds = ('0' + t.getSeconds()).slice(-2);
+    const time = `${date}/${month}/${year}, ${hours}:${minutes}:${seconds}`;
+    return time;
+  }
 }
